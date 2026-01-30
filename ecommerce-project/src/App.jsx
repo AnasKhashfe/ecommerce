303d602c34ab409fb623d7e269c787d21fb2c44a
AnasKhashfe <khashfeanas@gmail.com>
2026-01-30 19:06:11 +0300

    Create add to cart feature

--- ecommerce-project/src/App.jsx
+++ ecommerce-project/src/App.jsx
@@ -1,6 +1,6 @@
 import axios from 'axios'
 import { Routes, Route } from 'react-router'
-import { useState,useEffect } from 'react'
+import { useState, useEffect } from 'react'
 import { HomePage } from './pages/home/HomePage'
 import { CheckoutPage } from './pages/checkout/CheckoutPage'
 import { OrdersPage } from './pages/orders/OrdersPage'
@@ -12,46 +12,46 @@ import { NotFoundPage } from './pages/NotFoundPage'
 
 
 
+
+
 function App() {
 
   const [cart, setCart] = useState([]);
+  const loadCart = async () => {
+    const response = await axios.get('/api/cart-items?expand=product');
+    setCart(response.data);
+  }
   useEffect(() => {
 
-    const getAppData = async () => {
-      const response = await axios.get('/api/cart-items?expand=product');
-      setCart(response.data);
-    }
+    loadCart();
 
-    
-    getAppData();
-
-  },[])
+  }, [])
 
   return (
     <Routes>
       <Route
         index /*path = "/" Do the same thing as index prop but index prop is a shortcut*/
-        element={<HomePage cart = { cart } />}
+        element={<HomePage cart={cart} loadCart={loadCart} />}
       />
 
       <Route
         path="checkout"
-        element={<CheckoutPage cart = { cart } />}
+        element={<CheckoutPage cart={cart} />}
       />
 
       <Route
         path="Orders"
-        element={<OrdersPage cart = { cart } />}
+        element={<OrdersPage cart={cart} />}
       />
 
       <Route
         path="tracking/:orderId/:productId"
-        element={<TrackingPage cart = { cart }/>}
+        element={<TrackingPage cart={cart} />}
       />
 
       <Route
         path="*"
-        element={<NotFoundPage cart={cart}/>}
+        element={<NotFoundPage cart={cart} />}
       />
 
 
